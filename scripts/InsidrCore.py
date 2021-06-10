764460df4c918adcd3ab07062709a8d4400e2d04
Jay Rank <rankjay0101@gmail.com>
2021-06-10 22:01:33 +0530

    Update InsidrCore.py

--- scripts/InsidrCore.py
+++ scripts/InsidrCore.py
@@ -14,22 +14,39 @@ RETWEETERS_DETAILS = '{}'
 def InsidrAlgorithm():
     pass
 
+
+
+
 def Collectibles(retweetersID):
     global RETWEETERS_DETAILS
 
     TweetMetricValue, UserMetricValue = { retweetID[0]: tweetMetricComponent(retweetID[0]) for retweetID in retweetersID }, { retweeterID[1]: userMetricComponent(retweeterID[1]) for retweeterID in retweetersID }
-    UserMetricValue = { retweeter['user']['id']: [{"created_at": retweeter['user']['created_at']}, {"screen_name": retweeter['user']['screen_name']}, {"favourites_count": retweeter['user']['favourites_count']}, {"followers_count": retweeter['user']['followers_count']}, {"location": retweeter['user']['location']}, {"statuses_count": retweeter['user']['statuses_count']}] for retweeter in RETWEETERS_DETAILS }
+    # UserMetricValue = { retweeter['user']['id']: [{"created_at": retweeter['user']['created_at']}, {"screen_name": retweeter['user']['screen_name']}, {"favourites_count": retweeter['user']['favourites_count']}, {"followers_count": retweeter['user']['followers_count']}, {"location": retweeter['user']['location']}, {"statuses_count": retweeter['user']['statuses_count']}] for retweeter in RETWEETERS_DETAILS }
+    # print(TweetMetricValue, UserMetricValue)
 
     return TweetMetricValue, UserMetricValue
 
 def userMetricComponent(userID):
+    global RETWEETERS_DETAILS
+    global PARENT_USER_DETAILS
+    
+    PARENT_USER_DETAILS["favourites_count"] = RETWEETERS_DETAILS[0]['retweeted_status']['user']['favourites_count']
+    PARENT_USER_DETAILS["followers_count"] = RETWEETERS_DETAILS[0]['retweeted_status']['user']['followers_count']
+    PARENT_USER_DETAILS["friends_count"] = RETWEETERS_DETAILS[0]['retweeted_status']['user']['friends_count']
+    PARENT_USER_DETAILS["listed_count"] = RETWEETERS_DETAILS[0]['retweeted_status']['user']['listed_count']
+    PARENT_USER_DETAILS["statuses_count"] = RETWEETERS_DETAILS[0]['retweeted_status']['user']['statuses_count']
+
+    scrappedValues = '{}'
     unscrappedInfo = TwitterAPI.userMetrics(userID)
-    scrappedValues = [{"created_at": unscrappedInfo['data'][0]['created_at']}, {"name": unscrappedInfo['data'][0]['name']}, {"username": unscrappedInfo['data'][0]['username']}]
+    scrappedValues = [{"created_at": unscrappedInfo['data'][0]['created_at']}, {"name": unscrappedInfo['data'][0]['name']}, {"username": unscrappedInfo['data'][0]['username']}, {"public_metrics": [{"favourites_count": retweeter['user']['favourites_count']}, {"followers_count": retweeter['user']['followers_count']}, {"friends_count": retweeter['user']['friends_count']}, {"listed_count": retweeter['user']['listed_count']}, {"statuses_count": retweeter['user']['statuses_count']}] for retweeter in RETWEETERS_DETAILS }]
+    
     return scrappedValues
 
 def tweetMetricComponent(tweetID):
+    global RETWEETERS_DETAILS
+
     unscrappedInfo = TwitterAPI.tweetMetrics(tweetID)
-    scrappedValues = [{"author_id": unscrappedInfo['data'][0]['author_id']}, {"created_at": unscrappedInfo['data'][0]['created_at']}, {"public_metrics": unscrappedInfo['data'][0]['public_metrics']}]
+    scrappedValues = [{"author_id": unscrappedInfo['data'][0]['author_id']}, {"created_at": unscrappedInfo['data'][0]['created_at']}, {"public_metrics": unscrappedInfo['data'][0]['public_metrics']}, {"source": unscrappedInfo['data'][0]['source']}]
 
     return scrappedValues
 
@@ -44,14 +61,10 @@ class Utility():
     
     def retweetersScraping(self):
         global RETWEETERS_DETAILS
-        directoryPath = os.path.dirname(os.path.realpath(__file__))
-
 
         tweet_id = self.PARENT_TWEET_ID
         key = APISession.consumer_key
         secret = APISession.consumer_secret
-        timeElapsed = int(time())
-
 
         authenticationURL = "https://api.twitter.com/oauth2/token"
         data = {'grant_type': 'client_credentials'}
@@ -90,8 +103,10 @@ class Utility():
 
 def createInstance(PARENT_TWEET_ID):
     utilityInstance = Utility(PARENT_TWEET_ID)
-    # utilityInstance.parentDetailsFromTwitterAPI()
+    utilityInstance.parentDetailsFromTwitterAPI()
+    scrappedTweetMetrics, scrappedUsertMetrics = Collectibles(utilityInstance.retweetersScraping())
 
-    Collectibles(utilityInstance.retweetersScraping())
+    # print(json.dumps(PARENT_USER_DETAILS, indent=4, sort_keys=True))
+    # print(json.dumps(PARENT_TWEET_DETAILS, indent=4, sort_keys=True))
 
-createInstance(1400741886529179653)
+# createInstance(1397207890075832320)
